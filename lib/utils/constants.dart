12de4cfd93234ee51d74574c03111af5dd7f8fb8
Ronald Shiloli <ronaldshiloli@gmail.com>
2025-04-07 10:06:48 +0300

    changed color pallete

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,25 +5,25 @@ Color getTileColor(int value) {
     case 2:
       return Colors.deepPurple.shade100;
     case 4:
-      return Colors.deepPurple.shade200;
-    case 8:
       return Colors.deepPurple.shade300;
+    case 8:
+      return Color(0xFFFFAB91); // Coral
     case 16:
-      return Colors.deepPurple.shade400;
+      return Color(0xFFFF8A65); // Vibrant Orange
     case 32:
-      return Colors.deepPurple.shade500;
+      return Color(0xFF4DB6AC); // Teal
     case 64:
-      return Colors.deepPurple.shade600;
+      return Color(0xFFE57373); // Soft Red
     case 128:
-      return Colors.deepPurple.shade700;
+      return Color(0xFF1E88E5); // Blue 600
     case 256:
-      return Colors.deepPurple.shade800;
+      return Color(0xFFAED581); // Soft Green
     case 512:
-      return Colors.deepPurple.shade900;
+      return Color(0xFF8D6E63); // Dark Brown
     case 1024:
-      return Colors.teal.shade700;
+      return Color(0xFFD32F2F); // Dark Red
     case 2048:
-      return Colors.teal.shade800;
+      return Color(0xFF424242); // Dark Grey
     default:
       return Colors.grey[400]!;
   }
